61386353d4fb2fcc73f1ed54a36226953bdb0d44
Jayson Falkner <jfalkner@pacificbiosciences.com>
2016-11-11 12:29:23 -0800

    Multi-entry cache support and optimization

--- src/main/scala/com/pacb/itg/metrics/cache/MetricsCache.scala
+++ src/main/scala/com/pacb/itg/metrics/cache/MetricsCache.scala
@@ -1,7 +1,6 @@
 package com.pacb.itg.metrics.cache
 
 import java.nio.file.{Files, Path, Paths}
-import java.time.Instant
 
 import falkner.jayson.metrics._
 import falkner.jayson.metrics.io.CSV
@@ -13,9 +12,11 @@ import jfalkner.logs.Logs
 
 import scala.collection.mutable.Map
 
-case class Entry(key: String, version: String, value: Path)
+// cache files are looked up by (key, version) and path has many lines of CSV with index being the row for this entry
+case class Entry(key: String, version: String, value: Path, index: Int)
 
 case class Key(key: String, version: String)
+case class Value(p: Path, index: Int)
 
 object MetricsCache {
 
@@ -29,16 +30,17 @@ class MetricsCache(val cacheDir: Path) extends Logs {
   override val logsPath = Files.createDirectories(cacheDir.resolve("logs"))
 
   val entryLog = make[Entry](".entries.csv")
+  entryLog.squash()
 
-  lazy val cache: Map[Key, Path] = Map(entryLog.load().map(e => (Key(e.key, e.version), e.value)).toList :_ *)
+  lazy val cache: Map[Key, Value] = Map(entryLog.load().map(e => (Key(e.key, e.version), Value(e.value, e.index))).toList :_ *)
 
   def getOrElse(key: String, version: String, c: => Chunk): Chunk = getOrElse(Key(key, version), c)
 
   def getOrElse(key: Key, chunk: => Chunk): Chunk = cache.get(key) match {
-    case Some(hit) => Try(Files.readAllLines(hit).asScala.toList) match {
+    case Some(hit) => Try(Files.readAllLines(hit.p).asScala.toList) match {
       case Success(lines) =>
         //println(s"Matched: $key. Returning cached chunk.")
-        CSV(lines)
+        CSV(lines, hit.index)
       case Failure(t) =>
         //println(s"Failed converting cached chunk for: $key. Returning default chunk.")
         chunk
@@ -51,21 +53,46 @@ class MetricsCache(val cacheDir: Path) extends Logs {
   def updateIfNotEqual(key: String, m: => Metrics): Chunk = updateIfNotEqual(key, m.version, CSV(m))
 
   def updateIfNotEqual(key: String, version: String, chunk: Chunk): Chunk = cache.get(Key(key, version)) match {
-    case Some(hit) => Try(Files.readAllLines(hit).asScala.toList) match {
-      case Success(lines) => if (CSV(lines).all != chunk.all) makeEntry(key, version, chunk) else chunk
-      case Failure(t) => makeEntry(key, version, chunk)
-    }
+    case Some(hit) =>
+      val lines = Files.readAllLines(hit.p).asScala.toList
+      if (CSV(lines, hit.index).all != chunk.all) makeEntry(key, version, chunk) else chunk
     case _ => makeEntry(key, version, chunk)
   }
 
-  def makeEntry(key: String, m: Metrics): Chunk = makeEntry(key, m.version, CSV(m))
-
   def makeEntry(key: String, version: String, c: Chunk): Chunk = {
-    val ts = Instant.now().toString()
-    val p = Files.write(Files.createDirectories(cacheDir.resolve(version)).resolve(ts), c.all.getBytes)
-    cache(Key(key, version)) = p
-    entryLog.log(Entry(key, version, p))
+    val k = Key(key, version)
+    cache.get(k) match {
+      case Some(hit) =>
+        val lines = Files.readAllLines(hit.p).asScala.toList.patch(hit.index, Seq(c.values), 1)
+        Files.write(hit.p, lines.mkString("\n").getBytes)
+      case None =>
+        val p = latestBlock(version)
+        if (!Files.exists(p)) Files.createFile(p)
+        val index = Files.readAllLines(p).asScala.toList match {
+          case lines if lines.isEmpty =>
+            Files.write(p, c.all.getBytes)
+            1
+          case lines =>
+            Files.write(p, (lines ++ Seq(c.values)).mkString("\n").getBytes)
+            lines.size
+        }
+        cache(k) = Value(p, index)
+        entryLog.log(Entry(k.key, k.version, p, index))
+    }
     c
   }
-}
 
+  // default NFS rsize matches /pbi/dept/itg. find via `mount -l | grep itg`. Must be smaller to use just one block transfer
+  def latestBlock(version: String, sizeLimit: Long = 65536 - 1024): Path = {
+    val versionCache = cacheDir.resolve(version)
+    Files.createDirectories(versionCache)
+    val sorted = Files.list(versionCache).iterator.asScala.toList.sortWith(_.getFileName.toString > _.getFileName.toString)
+    sorted.isEmpty match {
+      case true => versionCache.resolve("0.csv")
+      case _ => Files.size(sorted.head) > sizeLimit match {
+        case true => versionCache.resolve(s"${sorted.head.getFileName.toString.split('.').head.toInt + 1 }.csv")
+        case _ => sorted.head
+      }
+    }
+  }
+}
--- src/test/scala/com/pacb/itg/metrics/cache/MetricsCacheSpec.scala
+++ src/test/scala/com/pacb/itg/metrics/cache/MetricsCacheSpec.scala
@@ -39,6 +39,46 @@ class MetricsCacheSpec extends Specification {
         mc2.cache mustEqual mc.cache
       }
     }
+    "Confirm multiple entries per cache file works" in {
+      withCleanup { cacheDir =>
+        val mc = MetricsCache(cacheDir)
+        val a = new TestMetrics("a", 1)
+        val b = new TestMetrics("b", 2)
+        val bc = CSV(b)
+        val c = new TestMetrics("c", 3)
+        val ka =  Key("ka", a.version)
+        val kb =  Key("kb", b.version)
+        val kc =  Key("kc", c.version)
+        mc.updateIfNotEqual(ka.key, a)
+        mc.updateIfNotEqual(kb.key, b.version, bc)
+        mc.updateIfNotEqual(kc.key, c)
+        Files.list(cacheDir.resolve(a.version)).iterator.asScala.toList.size mustEqual 1
+        mc.getOrElse(ka, null) mustEqual CSV(a)
+        mc.getOrElse(kb, null) mustEqual CSV(b)
+        mc.getOrElse(kc.key, kc.version, null) mustEqual CSV(c)
+        // confirm duplicate entries don't alter the cache
+        mc.updateIfNotEqual(kb.key, b.version, bc).eq(bc) mustEqual true
+        // confirm an in-place update works
+        val b2 = new TestMetrics("b", 22)
+        mc.updateIfNotEqual(kb.key, b2)
+        mc.getOrElse(kb, null) mustEqual CSV(b2)
+        // confirm a reload has the same -- also test String-based constructor
+        val mc2 = MetricsCache(cacheDir.toAbsolutePath.toString)
+        mc2.getOrElse(ka, null) mustEqual CSV(a)
+        mc2.getOrElse(kb, null) mustEqual CSV(b2)
+        mc2.getOrElse(kc, null) mustEqual CSV(c)
+        // confirm block size is working
+        mc.latestBlock(a.version) mustEqual mc.cache(ka).p
+        mc.latestBlock(a.version).getFileName.toString mustEqual "0.csv"
+        mc.latestBlock(a.version, 0).getFileName.toString mustEqual "1.csv"
+        // confirm block name sort is working -- e.g. if 3.csv exists, then 4.csv is made
+        Files.createFile(mc.latestBlock(a.version, 0))
+        mc.latestBlock(a.version, -1).getFileName.toString mustEqual "2.csv"
+        // if a file is deleted that the cache depends on, auto-recover
+//        Files.list(cacheDir.resolve(a.version)).iterator.asScala.toList.foreach(f => Files.delete(f))
+//        mc.getOrElse(ka, null) mustEqual CSV(a)
+      }
+    }
   }
 
   def withCleanup(f: (Path) => MatchResult[Any]): MatchResult[Any] = {
